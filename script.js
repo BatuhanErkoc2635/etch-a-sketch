d5390defb4b1bd26e64632c5b348d45e56e255c0
Batuhanerkoc2635 <batuhanerkocbuss@gmail.com>
2024-07-20 18:24:42 +0300

    Buttons fixed && banned <0 numbers

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector(".container")
 let gridNumber = prompt("Please enter the grid number: (do not enter more than 100)")
-
+const resizeButton = document.querySelector(".resizeButton")
 
 
 
 function createGrid(index){
-    if(gridNumber <= 100){
+    if(gridNumber <= 100 && gridNumber > 0){
         for (let i = 1; i <= index; i++) {
             const column = document.createElement("div")
             column.classList.add("column")
@@ -21,6 +21,10 @@ function createGrid(index){
             }
         }
     }
+    else if (gridNumber <= 0 ){
+        alert("Grid number cant be less than 1")
+        location.reload()
+    }
     else{
         alert("Please dont enter a number bigger than 100")
         location.reload()
@@ -29,18 +33,26 @@ function createGrid(index){
 
 
 function detectSquare(){
+    const clearButton = document.querySelector(".clearButton")
     squares = document.querySelectorAll(".row")
     console.log(squares)
     squares.forEach(element => {
         element.addEventListener ("mouseover" , () => {
             var randomColor = Math.floor(Math.random()*16777215).toString(16);
-            
             element.style.backgroundColor = "#" + randomColor
             
+        })
+        clearButton.addEventListener("click",() =>{
+            element.style.backgroundColor = "white"
         })
         
     });
 }
+
+resizeButton.addEventListener("click",() => {
+    location.reload()
+})
+
 
 
 createGrid(gridNumber)
